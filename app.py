5712b4f29c33737c6882b895f7992c24e55ed9a7
SIDDHARTH UPADHYAY <103942997+SID-9@users.noreply.github.com>
2024-02-04 00:46:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ model=pickle.load(open('model.pkl','rb'))
 @app.route('/')
 #retur if the above is true
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 @app.route('/predict',methods=['GET','POST'])
 def predict():
@@ -27,16 +27,13 @@ def predict():
         
         output = round(prediction[0],2) # this will return outout as number instead of a list rounded upto 2 decimal places
         print(output)
-        return render_template("index.html",prediction_text = f'Salary predicted is : Rs. {output}/-')
+        return render_template('index.html',prediction_text = f'Salary predicted is : Rs. {output}/-')
     
     except ValueError as e:
         # Handle the case where user didn't provide valid numeric input
         error_message = "Invalid input. Please enter numeric values."
-        return render_template("index.html", prediction_error=error_message)
+        return render_template('index.html', prediction_error=error_message)
 
-
-if __name__=='__main__':
-    app.run(debug=True)
 
 
 
